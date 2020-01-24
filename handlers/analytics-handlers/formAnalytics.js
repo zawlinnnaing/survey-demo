e6269ef53928625e7e2d1b00361225a4b0b66319
zawlinnnaing <zawlinnnaing0018@gmail.com>
2020-01-24 20:40:05 +0630

    completed excel for form data

--- handlers/analytics-handlers/formAnalytics.js
+++ handlers/analytics-handlers/formAnalytics.js
@@ -2,6 +2,7 @@ let models = require("../../models");
 // const excel = require("node-excel-export");
 const x1 = require("excel4node");
 const { listQuestionsTypes, textQuestionTypes } = require("../../config/app");
+const fs = require("fs");
 
 module.exports = async (req, res, next) => {
   let formId = req.params.formId;
@@ -36,12 +37,14 @@ module.exports = async (req, res, next) => {
     // console.log(typeof report);
     // res.attachment("report.xlsx");
     // res.sendFile(report);
-    res.setHeader("Content-Type", "application/vnd.openxmlformats");
-    res.setHeader(
-      "Content-Disposition",
-      "attachment; filename=" + `${form.title}.xlsx`
-    );
-    res.end(buildExcel(form));
+    // res.setHeader("Content-Type", "application/vnd.openxmlformats");
+    // res.setHeader(
+    //   "Content-Disposition",
+    //   "attachment; filename=" + `${form.title}.xlsx`
+    // );
+    // res.end(buildExcel(form));
+    buildExcel(form, res);
+    // res.end();
   } catch (e) {
     console.error(e);
     res.status(500).json({
@@ -56,64 +59,71 @@ module.exports = async (req, res, next) => {
  *
  * @param {Form} form
  */
-function buildExcel(form) {
+async function buildExcel(form, res) {
   let workbook = new x1.Workbook();
-  let worksheet = workbook.addWorksheet(form.title);
-  return workbook.write("test" + ".xlsx");
-  //   let headings = [];
-  //   let specification = {};
-  //   let questionsAnswers = {};
-  //   form.questions.map((question, key) => {
-  //     let questionTitle = question.question;
-  //     headings.push({
-  //       value: questionTitle,
-  //       style: styles.headerDark
-  //     });
-  //     if (listQuestionsTypes.includes(question.type)) {
-  //       questionsAnswers[questionTitle] = question.listAnswers.map(ele => {
-  //         return ele.Items.map(ele => {
-  //           return ele.itemName;
-  //         });
-  //       });
-  //     } else if (textQuestionTypes.includes(question.type)) {
-  //       questionsAnswers[questionTitle] = question.answers.map(ele => {
-  //         return ele.answer;
-  //       });
-  //     }
-  //     specification[questionTitle] = {
-  //       displayName: questionTitle,
-  //       headerStyle: styles.headerDark,
-  //       cellStyle: () => {
-  //         return key % 2 ? styles.cellBlue : styles.cellBlueDark;
-  //       },
-  //       width: String(questionTitle).length
-  //     };
-  //   });
+  let worksheet = workbook.addWorksheet(String(form.title), {
+    sheetFormat: {}
+  });
+  let style = workbook.createStyle({
+    font: {
+      color: "#FFFFFF",
+      size: 12
+    }
+  });
+  let questionsAnswers = {};
+  form.questions.map((question, key) => {
+    let questionTitle = question.question;
+    if (listQuestionsTypes.includes(question.type)) {
+      questionsAnswers[questionTitle] = question.listAnswers.map(ele => {
+        return ele.Items.map(ele => {
+          return ele.itemName;
+        });
+      });
+    } else if (textQuestionTypes.includes(question.type)) {
+      questionsAnswers[questionTitle] = question.answers.map(ele => {
+        return ele.answer;
+      });
+    }
+  });
 
-  //   //   console.log(questionsAnswers);
-  //   //   console.log(mergedArray(questionsAnswers));
-  //   let dataset = mergedArray(questionsAnswers);
-  //   //   unset()
-  //   questionsAnswers = undefined;
-  //   let merges = [];
-  //   dataset.map((ele, index) => {
-  //     let tempObj = {
-  //       start: { row: ++index, column: 1 },
-  //       end: { row: index, column: form.questions.length }
-  //     };
-  //     merges.push(tempObj);
-  //   });
-  //   console.log(merges);
-  //   const report = excel.buildExport([
-  //     {
-  //       name: form.title,
-  //       heading: headings,
-  //       merges: merges,
-  //       specification: specification,
-  //       data: dataset
-  //     }
-  //   ]);
-  //   return report;
+  // console.log(questionsAnswers);
+
+  let dataset = mergedArray(questionsAnswers);
+  console.log(dataset);
+
+  let row = 1;
+  let column = 1;
+  Object.keys(dataset[0]).map(key => {
+    // Setting question as header for worksheet
+    worksheet.cell(row, column).string(String(key));
+    column++;
+  });
+  row++, (column = 1);
+  dataset.map(ele => {
+    // Get each element from array dataset
+    (column = 1), (cellStyle = {});
+    Object.keys(ele).map(key => {
+      // Get each key from element object
+      if (column % 2 == 0) {
+        cellStyle.fill = {
+          type: "pattern",
+          fgColor: "#90CAF9"
+        };
+      } else {
+        cellStyle.fill = {
+          type: "pattern",
+          fgColor: "#0D47A1"
+        };
+      }
+      worksheet
+        .cell(row, column)
+        .string(ele[key])
+        .style(cellStyle);
+      column++;
+    });
+    row++;
+  });
+  workbook.write(`${form.title}.xlsx`, res);
 }
 
 /**
@@ -124,17 +134,30 @@ function buildExcel(form) {
  */
 function mergedArray(allAnswers) {
   let finalArray = [];
-  let arrayLength = Object.keys(allAnswers)[0].length;
+  let arrayLength = allAnswers[Object.keys(allAnswers)[0]].length;
   for (let i = 0; i < arrayLength; i++) {
     let tempObj = {};
     Object.keys(allAnswers).map(key => {
+      console.log(
+        "key : ",
+        key,
+        "\n",
+        "index :",
+        i,
+        "\n",
+        "value :",
+        allAnswers[key][i],
+        "\n",
+        "Instance: ",
+        typeof allAnswers[key][i]
+      );
       if (
         allAnswers[key][i] instanceof Array &&
         allAnswers[key][i].length > 0
       ) {
         tempObj[key] = allAnswers[key][i].toString();
-      } else if (allAnswers[key][i] instanceof String) {
-        tempObj[key] = allAnswers[key][i];
+      } else if (allAnswers[key][i]) {
+        tempObj[key] = String(allAnswers[key][i]);
       } else {
         tempObj[key] = "";
       }
